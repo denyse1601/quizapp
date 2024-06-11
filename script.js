3d2d4511a37b8ffd54658126128bfb79c708b247
denyse1601 lima <denysepessoainha@gmail.com>
2024-06-11 04:45:38 +0000

    mais um ajuste no resultado.css

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-import {trocarTema, verificarTema} from "./helpers/tema-helper.js"
+import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
+
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 
 botaoTema.addEventListener("click", () => {
-    trocarTema(body,botaoTema)
+    trocarTema(body, botaoTema)
 })
 
-verificarTema(body,botaoTema)
+verificarTema (body, botaoTema)
 
 const botoesAssunto = document.querySelectorAll(".assuntos button")
 botoesAssunto.forEach(botao => {
     botao.addEventListener("click", selecionarAssunto)
 })
 
-function selecionarAssunto (evento) {
+function selecionarAssunto(evento) {
     const classeBotao = evento.target.className
     const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
